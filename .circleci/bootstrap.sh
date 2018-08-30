f44f9b36536962f460d1618f9b3e53ae41616345
Shao Cheng <cheng.shao@tweag.io>
2018-08-31 04:16:06 +0800

    no message

---
+++ .circleci/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+
+apt update
+apt full-upgrade -y
+apt install -y \
+  curl \
+  gnupg
+
+curl -sSL https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb stable main" > /etc/apt/sources.list.d/google-chrome-unstable.list
+
+apt update
+apt install -y \
+  fonts-noto* \
+  google-chrome-unstable
+
+apt remove -y \
+  curl \
+  gnupg
+apt autoremove -y
+apt clean -y
+rm /tmp/bootstrap.sh
